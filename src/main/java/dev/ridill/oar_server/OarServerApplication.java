45d8526c37ecea7c8896db82f5a1085e1903e934
RemijiusBrian <remijius.b@applab.qa>
2026-07-05 10:21:19 +0300

    Initial commit

---
+++ src/main/java/dev/ridill/oar_server/OarServerApplication.java
@@ -0,0 +1,13 @@
+package dev.ridill.oar_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ridill/oar_server/OarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ridill.oar_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
